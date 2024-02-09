bcd9df0e1fe4d68f7553ad7e2211872764193d6c
atlantisgroupbv <newcityofatlantis@gmail.com>
2024-02-09 16:06:18 +0100

    done

--- SuzieBundle.php
+++ SuzieBundle.php
@@ -2,7 +2,7 @@
 
 namespace KooijmanInc\SuzieBundle;
 
-use KooijmanInc\Suzie\SuzieFactory;
+use KooijmanInc\Suzie\Service\SuzieFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SuzieBundle extends Bundle
